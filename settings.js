f62accb6441ce52cf7a575d1396951669a8d8624
eaftmcclure <165178031+eaftmcclure@users.noreply.github.com>
2024-03-27 11:29:28 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,444 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 410,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": false,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "vcg",
+ "map_data": {
+  "st1": {
+   "name": "Alabama",
+   "shortname": "AL",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st2": {
+   "name": "Alaska",
+   "shortname": "AK ",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st3": {
+   "name": "Arizona",
+   "shortname": "AZ",
+   "comment": "<p>Campaigns:</p><ul><li>Defense</li></ul><p><br></p><p>Ballots:</p><ul><li>2040 (no public money)</li><li>2050 </li></ul><p><br></p><p>Political Plan</p><ul><li>State Lege</li><li>PUC</li></ul>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/arizona?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st4": {
+   "name": "Arkansas",
+   "shortname": "AR",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st5": {
+   "name": "California",
+   "shortname": "CA",
+   "comment": "<p>Campaigns: </p><ul><li>Building Decarb</li><li>OSW</li><li>Transmission</li><li>Polluter Accountability</li><li>Budget Defense / Climate Bond</li></ul><p><br></p><p>Ballots:</p><ul><li>Climate Bond</li><li>Referrendum</li></ul>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/california?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st6": {
+   "name": "Colorado",
+   "shortname": "CO",
+   "comment": "<p>Campaigns: </p><ul><li>Ozone reform (3 bills)</li><li>Siting</li><li>Buildings</li></ul><p><br></p><p>Ballots:</p><ul><li>Many oil/gas ballots</li></ul>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/colorado?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st7": {
+   "name": "Connecticut",
+   "shortname": "CT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st8": {
+   "name": "Delaware",
+   "shortname": "DE",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st9": {
+   "name": "District of Columbia",
+   "shortname": "DC",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st10": {
+   "name": "Florida",
+   "shortname": "FL",
+   "comment": "<p>Campaigns:</p><ul><li>Defense (pre-emption /siting)</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/florida?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st11": {
+   "name": "Georgia",
+   "shortname": "GA",
+   "comment": "<p>Campaigns:</p><ul><li>EJ Study Cmt</li></ul><p><br></p><p>Political Plan </p><ul><li>PUC</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/georgia?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st12": {
+   "name": "Hawaii",
+   "shortname": "HI",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st13": {
+   "name": "Idaho",
+   "shortname": "ID",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st14": {
+   "name": "Illinois",
+   "shortname": "IL",
+   "comment": "<p>Campaigns:</p><ul><li>Transpo </li><li>Clean Heat</li></ul><p><br></p><p><br></p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/illinois?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st15": {
+   "name": "Indiana",
+   "shortname": "IN",
+   "comment": "<p>Campaigns: </p><ul><li>Defense</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/indiana?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st16": {
+   "name": "Iowa",
+   "shortname": "IA",
+   "comment": "<p>Campaigns</p><ul><li>Defense / utility rewrite</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/iowa?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st17": {
+   "name": "Kansas",
+   "shortname": "KS",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st18": {
+   "name": "Kentucky",
+   "shortname": "KY",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st19": {
+   "name": "Louisiana",
+   "shortname": "LA",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st20": {
+   "name": "Maine",
+   "shortname": "ME",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st21": {
+   "name": "Maryland",
+   "shortname": "MD",
+   "comment": "<p>Campaigns:</p><ul><li>Transportation</li><li>Equity Budget</li><li>EmPower</li></ul><p><br></p><p><br></p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/maryland?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st22": {
+   "name": "Massachusetts",
+   "shortname": "MA",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st23": {
+   "name": "Michigan",
+   "shortname": "MI",
+   "comment": "<p>Campaigns:</p><ul><li>Budget implementation </li></ul><p><br></p><p>Political Plan</p><p><br></p><p>Ballots:</p><ul><li>siting repeal</li></ul>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/michigan?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st24": {
+   "name": "Minnesota",
+   "shortname": "MN",
+   "comment": "<p>Campaigns:</p><ul><li>Siting</li><li>Buildings</li></ul><p><br></p><p>Political Plan</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/minnesota?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st25": {
+   "name": "Mississippi",
+   "shortname": "MS",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st26": {
+   "name": "Missouri",
+   "shortname": "MO",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st27": {
+   "name": "Montana",
+   "shortname": "MT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st28": {
+   "name": "Nebraska",
+   "shortname": "NE",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st29": {
+   "name": "Nevada",
+   "shortname": "NV",
+   "comment": "<p>Political Plan</p><p><br></p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/nevada?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st30": {
+   "name": "New Hampshire",
+   "shortname": "NH",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st31": {
+   "name": "New Jersey",
+   "shortname": "NJ",
+   "comment": "<p>Campaigns:</p><ul><li>100% Clean</li></ul>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/new-jersey?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st32": {
+   "name": "New Mexico",
+   "shortname": "NM",
+   "comment": "<p>Campaigns:</p><ul><li>Fed Matching Fund</li></ul><p><br></p><p>Political Plan</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/new-mexico?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st33": {
+   "name": "New York",
+   "shortname": "NY",
+   "comment": "<p>Campaigns: </p><ul><li>Equity Budget</li><li>Clean Heat</li></ul><p><br></p><p><br></p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/new-york?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st34": {
+   "name": "North Carolina",
+   "shortname": "NC",
+   "comment": "<p>Campaigns:</p><ul><li>Defense</li><li>Market Reform</li></ul><p><br></p><p>Political Plan</p><ul><li>State Lege</li><li>Governor</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/north-carolina?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st35": {
+   "name": "North Dakota",
+   "shortname": "ND",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st36": {
+   "name": "Ohio",
+   "shortname": "OH",
+   "comment": "<p>Ballots:</p><ul><li>Redistricting commission</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/ohio?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st37": {
+   "name": "Oklahoma",
+   "shortname": "OK",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st38": {
+   "name": "Oregon",
+   "shortname": "OR",
+   "comment": "<p>Campaigns:</p><p><br></p><ul><li>OSW</li></ul>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/oregon?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st39": {
+   "name": "Pennsylvania",
+   "shortname": "PA",
+   "comment": "<p>Campaigns: </p><ul><li>AEPS Rewrite</li><li>RGGI / PACER </li><li>Whole Home Repairs</li></ul><p><br></p><p>Political Plan</p><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/pennsylvania?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st40": {
+   "name": "Rhode Island",
+   "shortname": "RI",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st41": {
+   "name": "South Carolina",
+   "shortname": "SC",
+   "comment": "<p>Campaigns:</p><ul><li>Market Reform</li><li>Defense</li><li>Energy For All (C4ward)</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/south-carolina?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st42": {
+   "name": "South Dakota",
+   "shortname": "SD",
+   "comment": "",
+   "link": "https://docs.google.com/spreadsheets/d/1cIcFlIoqD5sDRqMJAmPtg9_Z0GHUzK4Yo1GFcaCyBsM/edit#gid=0",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st43": {
+   "name": "Tennessee",
+   "shortname": "TN",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st44": {
+   "name": "Texas",
+   "shortname": "TX",
+   "comment": "<p>Campaigns:</p><ul><li>Texas ambitions</li></ul><p><br></p><p>Political Plan</p><ul><li>100k to Speaker</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/texas?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st45": {
+   "name": "Utah",
+   "shortname": "UT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st46": {
+   "name": "Vermont",
+   "shortname": "VT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st47": {
+   "name": "Virginia",
+   "shortname": "VA",
+   "comment": "<p>Campaigns:</p><ul><li>Clean Cars Defense</li><li>VCEA Implementation</li><li>Siting</li></ul><p><br></p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/virginia?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st48": {
+   "name": "Washington",
+   "shortname": "WA",
+   "comment": "<p>Ballots:</p><ul><li>2021 Climate Law repeal</li></ul>",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st49": {
+   "name": "West Virginia",
+   "shortname": "WV",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st50": {
+   "name": "Wisconsin",
+   "shortname": "WI",
+   "comment": "<p>Campaigns: </p><ul><li>100% Clean (multi-year)</li></ul><p><br></p><p>Political Plan</p><p><br></p><p>Ballots:</p><ul><li>Executive authority fed investment</li></ul><p><br></p><p>C4ward State</p><p>C4ward State</p>",
+   "link": "https://sites.google.com/ef.org/eafstates/home/wisconsin?authuser=0",
+   "color": "#2B78E4",
+   "colorOver": "#ace16b"
+  },
+  "st51": {
+   "name": "Wyoming",
+   "shortname": "WY",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  }
+ },
+ "points": {}
+};
